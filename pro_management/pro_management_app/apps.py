70ec4323b6823037080c537a676f8a17a8061839
paramita <mitra3233@gmail.com>
2018-10-29 11:45:26 -0500

    sign up and login ready

---
+++ pro_management/pro_management_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProManagementAppConfig(AppConfig):
+    name = 'pro_management_app'
---
+++ pro_management/pro_management_app/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    # add additional fields in here
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    username = models.CharField(max_length=30,unique=True)
+    department = models.CharField(max_length=30)
+    date_of_join = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email

dc266878f3e90c3caf8dd511d70b6607ca06eacd
paramita <mitra3233@gmail.com>
2018-10-30 14:06:42 -0500

    Listview created successfull

--- pro_management/pro_management_app/urls.py
+++ pro_management/pro_management_app/urls.py
@@ -1,7 +1,7 @@
 # users/urls.py
 from django.urls import path
-from . import views
+from . import views,models
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-]
+    path('emp_details/', views.EmpDetails.as_view(), name='emp_details')]
--- pro_management/pro_management_app/views.py
+++ pro_management/pro_management_app/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import CustomUser
 
 from .forms import CustomUserCreationForm
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('emp_details')
     template_name = 'signup.html'
+
+
+class EmpDetails(generic.ListView):
+    model = CustomUser
+    template_name = 'emp_details.html'
+
